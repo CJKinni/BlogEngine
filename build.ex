a3bdbeb7c70237de55336426e8c54b5705759585
C.J.Kinniburgh <chris@cjkinni.com>
2021-04-09 23:19:00 -0500

    Correctly Order Posts Descending

--- build.ex
+++ build.ex
@@ -1,4 +1,5 @@
 BlogEngine.get_folder_contents("./words") |>
+Enum.reverse() |>
 Enum.map(fn x -> BlogEngine.read_blogpost(x) end) |>
 Enum.map(fn x -> BlogEngine.split_header(x) end) |>
 Enum.map(fn x -> BlogEngine.write_post(x) end) |>
